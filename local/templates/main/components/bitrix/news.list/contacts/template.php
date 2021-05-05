8e600afe247cbc9e93ffaf60a40c7311243840fc
aavday <wrck7@yandex.ru>
2021-05-05 19:08:48 +0300

    Исправил неотображающийся адрес в контактах

--- local/templates/main/components/bitrix/news.list/contacts/template.php
+++ local/templates/main/components/bitrix/news.list/contacts/template.php
@@ -9,7 +9,7 @@ if (!empty($arResult)):
             <div class="contact__block" id="<?=$this->GetEditAreaId($arResult['ITEMS'][0]['ID'])?>">
                 <h3 class="contact__block__title">Контакты</h3>
                 <h4 class="contact__block__town">
-                    <?=$arResult['ITEMS'][0]['DISPLAY_PROPERTIES']['ADDRESS_FULL']['VALUE']['HTML']?>
+                    <?=$arResult['ITEMS'][0]['DISPLAY_PROPERTIES']['ADDRESS_FULL']['~VALUE']['TEXT']?>
                 </h4>
                 <a class="contact__block__phone" href="tel:<?=$arResult['ITEMS'][0]['DISPLAY_PROPERTIES']['PHONE']['VALUE']?>">
                     <?=$arResult['ITEMS'][0]['DISPLAY_PROPERTIES']['PHONE']['VALUE']?>
