f5ba3e29320203b9bf965b772ed234074a2a3d67
aavday <wrck7@yandex.ru>
2021-05-07 18:40:40 +0300

    Добавил шаблон news.detail для копирайта в футере

---
+++ local/templates/main/components/bitrix/news.detail/copyright/template.php
@@ -0,0 +1,8 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+if (!empty($arResult)):
+?>
+    <p class="copyright">
+        <?=$arResult['DISPLAY_PROPERTIES']['COPYRIGHT']['VALUE']?>
+    </p>
+<?endif?>
