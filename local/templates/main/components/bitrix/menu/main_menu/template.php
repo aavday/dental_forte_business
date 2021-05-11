182d097c265d0a934f122fc7a262e0139d6f5745
aavday <wrck7@yandex.ru>
2021-05-11 17:33:07 +0300

    сделал скролл до секций при нажатии на ссылки в меню

--- local/templates/main/components/bitrix/menu/main_menu/template.php
+++ local/templates/main/components/bitrix/menu/main_menu/template.php
@@ -6,7 +6,9 @@
             if ($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
                 continue;
         ?>
-            <li class="nav-item"><a class="nav-link" href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
+                </li>
         <? endforeach; ?>
     </ul>
 <? endif; ?>
--- local/templates/main/js/template.js
+++ local/templates/main/js/template.js
@@ -1,14 +1,40 @@
+var anchors = document.querySelectorAll('.header .navbar-nav .nav-link');
+var navbar = document.querySelector('.header .navbar-collapse');
+var menu = document.querySelector('.header .menu');
+
+anchors.forEach(anchor => {
+    anchor.addEventListener('click', event => {
+        event.preventDefault();
+        var margin = parseInt(getComputedStyle(main).marginTop);
+        var section = document.querySelector(anchor.getAttribute('href').replace('/', '.'));
+        var bodyRect = document.body.getBoundingClientRect().top;
+        var sectionRect = section.getBoundingClientRect().top;
+        var sectionPosition = sectionRect - bodyRect;
+        var marginPosition = sectionPosition - margin;
+
+        if (navbar.classList.contains('show')) {
+            navbar.classList.remove('show');
+            menu.classList.toggle('back');
+        }
+
+        scrollTo({
+            top: marginPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
 function yandexMap() {
   try {
       let iconUrl = location.origin + "/local/templates/main/images/icons/map-icon.png";
 
       if ($(window).width() <= 992) {
           ymaps.ready(() => {
-              const myMap = new ymaps.Map("map-card", {
+              var myMap = new ymaps.Map("map-card", {
                   center: [55.756606, 52.435437],
                   zoom: 17,
               });
-              const myPlacemark = new ymaps.Placemark(
+              var myPlacemark = new ymaps.Placemark(
                   [55.755515, 52.435534],
                   {
                       hintContent: "",
@@ -31,11 +57,11 @@ function yandexMap() {
           });
       } else {
           ymaps.ready(() => {
-              const myMap = new ymaps.Map("map-card", {
+              var myMap = new ymaps.Map("map-card", {
                   center: [55.755588, 52.437834],
                   zoom: 17,
               });
-              const myPlacemark = new ymaps.Placemark(
+              var myPlacemark = new ymaps.Placemark(
                   [55.755515, 52.435534],
                   {
                       hintContent: "",
